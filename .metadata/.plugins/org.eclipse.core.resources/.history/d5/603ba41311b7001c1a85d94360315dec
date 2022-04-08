c58a9417e15b578d69e5102049cb4ee9c60bc7e5
Victor Pena <victorpenawow@gmail.com>
2022-04-08 14:54:52 +0200

    Primer Commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d5/603ba41311b7001c1a85d94360315dec
@@ -0,0 +1,5 @@
+package com.bolsadeideas.springboot.di.app.controllers;
+
+public class IndexController {
+
+}
---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,17 @@
+package com.bolsadeideas.springboot.di.app.models.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+
+//@Component("miServicioSimple")
+//@Primary
+public class MiServicio implements IServicio{
+	
+	@Override
+	public String operacion() {
+		return "ejecutando algun proceso importante simple...";
+	}
+	
+	
+}
